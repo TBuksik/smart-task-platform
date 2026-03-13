0b36a01394032fe5a96477f0e298ab2c62e8e884
GramarNazi <timok.pl001@gmail.com>
2026-03-13 13:18:58 +0100

    Add all security logic to app/core/security.py

--- app/core/security.py
+++ app/core/security.py
@@ -1,4 +1,16 @@
 from datetime import datetime, timedelta
 from typing import Optional
+from warnings import deprecated
 
 from jose import JWTError, jwt
+from passlib.context import CryptContext
+
+from app.core.config import settings
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def hash_password(password: str) -> str:
+    return pwd_context.hash(password)
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
