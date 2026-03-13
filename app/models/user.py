d52ef84e484451aef8e0845a8a85cc0d61b0d832
GramarNazi <timok.pl001@gmail.com>
2026-03-13 12:57:10 +0100

    add User SQLAlchemy model

---
+++ app/models/user.py
@@ -0,0 +1,38 @@
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, func
+from datetime import datetime
+
+from app.core.database import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(
+        primary_key=True,
+        autoincrement=True
+    )
+
+    email: Mapped[str] = mapped_column(
+        String(255),
+        nullable=False,
+        unique=True
+    )
+
+    hashed_password: Mapped[str] = mapped_column(
+        String(255),
+        nullable=False
+    )
+
+    is_active: Mapped[bool] = mapped_column(
+        Boolean(),
+        default=True,
+        nullable=False
+    )
+
+    created_at: Mapped[datetime] = mapped_column(
+        nullable=False,
+        server_default=func.now()
+    )
+
+    def __repr__(self) -> str:
+        return f"<User id={self.id} email={self.email}>"
