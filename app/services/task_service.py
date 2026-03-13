6e38d75d1b09aebdc8173e2b2c8dea9fcef361ec
GramarNazi <timok.pl001@gmail.com>
2026-03-13 11:23:12 +0100

    build functions with the query SELECT * FROM tasks

--- app/services/task_service.py
+++ app/services/task_service.py
@@ -2,9 +2,18 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from app.models.task import Task
 from app.schemas.task import TaskCreate
 
+from sqlalchemy import select
+from typing import Optional
+
 async def create_task(db: AsyncSession, task_data: TaskCreate) -> Task:
     db_task = Task(**task_data.model_dump())
     db.add(db_task)
     await db.commit()
     await db.refresh(db_task)
     return db_task
+
+async def get_task(db: AsyncSession, task_id: int) -> Optional[Task]:
+    result = await db.execute(
+        select(Task).where(Task.id == task_id)
+    )
+    return result.scalars().first()
