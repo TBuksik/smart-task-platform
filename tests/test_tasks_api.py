533adc6c41d9fb4e40f019fa322cf2beea74c3aa
GramarNazi <timok.pl001@gmail.com>
2026-03-22 23:20:24 +0100

    Add a test called 	est_get_tasks to 	est_tasks_api.py that verifies that GET /api/v1/tasks/ returns a list of tasks

--- tests/test_tasks_api.py
+++ tests/test_tasks_api.py
@@ -25,3 +25,21 @@ async def test_create_task(client: AsyncClient):
     assert data["id"] is not None
     assert data["created_at"] is not None
 
+async def test_get_tasks(client: AsyncClient):
+    token = await get_auth_token(client)
+
+    headers = {"Authorization": f"Bearer {token}"}
+
+    response_create = await client.post(
+        "/api/v1/tasks/",
+        headers=headers,
+        json={
+            "title": "Testowe zadanie",
+            "description": "Opis zadania",
+            "schedule": "codziennie o 8:00"
+        }
+    )
+
+    response_get = await client.get(
+        "/api/v1/auth"
+    )
