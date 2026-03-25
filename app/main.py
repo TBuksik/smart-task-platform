20f8e38294e5ba0d8af766061755b4586ac70689
GramarNazi <timok.pl001@gmail.com>
2026-03-25 21:11:04 +0100

    register WebSocket router in main application

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from app.core.config import settings
 from app.api.v1 import tasks, auth
+from app.api.v1 import websockets
 
 from prometheus_fastapi_instrumentator import Instrumentator
 from starlette.middleware.sessions import SessionMiddleware
@@ -15,6 +16,7 @@ app = FastAPI(
 
 app.include_router(tasks.router, prefix="/api/v1")
 app.include_router(auth.router, prefix="/api/v1")
+app.include_router(websockets.router)
 
 app.add_middleware(
     SessionMiddleware,
