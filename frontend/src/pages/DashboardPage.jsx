bdfd4fda10e96f5852e76d2e3fc0e216b9f52012
GramarNazi <timok.pl001@gmail.com>
2026-04-02 22:58:24 +0200

    react: add CSS Modules for DashboardPage

--- frontend/src/pages/DashboardPage.jsx
+++ frontend/src/pages/DashboardPage.jsx
@@ -1,17 +1,20 @@
 import TaskList from '../components/TaskList'
 import AddTaskForm from '../components/AddTaskForm'
-import Notification from '../components/Notifications'
+import Notifications from '../components/Notifications'
+import styles from './DashboardPage.module.css'
 
 function DashboardPage({ tasks, notifications, onAdd, onLogout }) {
-    return (
-        <div>
-            <h1>Dashboard</h1>
-            <button onClick={onLogout}>Wyloguj</button>
-            <AddTaskForm onAdd={onAdd}/>
-            <TaskList taskList={tasks}/>
-            <Notification notifications={notifications}/>
-        </div>
-    )
+  return (
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <h1 className={styles.title}>Dashboard</h1>
+        <button className={styles.button} onClick={onLogout}>Wyloguj</button>
+      </div>
+      <AddTaskForm onAdd={onAdd} />
+      <TaskList taskList={tasks} />
+      <Notifications notifications={notifications} />
+    </div>
+  )
 }
 
 export default DashboardPage
