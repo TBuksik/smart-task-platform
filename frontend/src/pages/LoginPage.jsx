b46f6b6d3bb5aff2c99ca6d8a6c36c7cd2723dbf
GramarNazi <timok.pl001@gmail.com>
2026-04-02 22:53:36 +0200

    react: add CSS Modules for LoginPage

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,14 +1,17 @@
-import LoginForm from "../components/LoginForm";
+import LoginForm from '../components/LoginForm'
+import styles from './LoginPage.module.css'
 
 function LoginPage({ onLogin, error, loading }) {
-    return (
-        <div>
-            <h1>Smart Task Platform</h1>
-            {loading && <p>Ładowanie...</p>}
-            {error && <p>{error}</p>}
-            <LoginForm onLogin={onLogin}/>
-        </div>
-    )
+  return (
+    <div className={styles.container}>
+      <div className={styles.card}>
+        <h1 className={styles.title}>Smart Task Platform</h1>
+        {loading && <p>Ładowanie...</p>}
+        {error && <p className={styles.error}>{error}</p>}
+        <LoginForm onLogin={onLogin} />
+      </div>
+    </div>
+  )
 }
 
 export default LoginPage
