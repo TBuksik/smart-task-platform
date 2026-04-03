f98c4a869a3a87a35fdd330247abf0e9474b3afc
GramarNazi <timok.pl001@gmail.com>
2026-04-03 22:38:51 +0200

    backend: add slowapi rate limiter

--- app/main.py
+++ app/main.py
@@ -1,10 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from slowapi import Limiter, _rate_limit_exceeded_handler
+from slowapi.util import get_remote_address
+from slowapi.errors import RateLimitExceeded
 from app.core.config import settings
 from app.api.v1 import tasks, auth
 from app.api.v1 import websockets
 
 from prometheus_fastapi_instrumentator import Instrumentator
 from starlette.middleware.sessions import SessionMiddleware
+
+limiter = Limiter(key_func=get_remote_address)
 
 app = FastAPI(
     title=settings.APP_NAME,
@@ -22,6 +27,9 @@ app.add_middleware(
     SessionMiddleware,
     secret_key=settings.SECRET_KEY
 )
+
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 Instrumentator().instrument(app).expose(app)
 
